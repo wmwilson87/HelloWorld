31ca5f99766bc087c4ffe387041ebb333bdb14d2
wmwilson87 <42006342+wmwilson87@users.noreply.github.com>
2022-03-10 11:25:48 -0700

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -1,4 +1,30 @@
 import sys
+import requests
 
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
+
+sha = ""
+if len(sys.argv) >= 1: 
+  sha = sys.argv[1]
+  url = r"https://api.github.com/repos/wmwilson87/HelloWorld/commits/" + sha
+  author = ""
+  touchedFiles = []
+  receive = requests.get(url)
+  if receive.status_code == 200:
+    ret = recieve.json()
+    if "author" in ret.keys() and "login" in ret["author"]:
+      author = ret["author"]["login"]
+    if "files" in ret.keys():
+      for f in ret["files"]:
+        if "filename" in f: touchedFiles.append(f["filename"])
+    
+    print("\n\n'%s' modified files [\n%s\n]" %(author, ',\n'.join(touchedFiles)))
+    
+  else:
+    print("url: '%s' error in request. ending." %url)
+else:
+  print("sha not included. ending.")
+  return -1
+
+
