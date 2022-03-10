8e2d49b161cf6d246e6f66b016725e2c70602b5c
Designworks\wwilson <wwilson@designworksgaming.com>
2022-03-10 16:07:28 -0700

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -27,27 +27,40 @@ if len(sys.argv) >= 1:
 else:
   print("sha not included.")
 
-touchedDirs = [] 
+touched = {}
+touchedDirs = []
 for tF in touchedFiles:
-  tF_split = tF.split("/")
-  if len(tF_split) >= 1:
-    full_tF = os.path.join(tF_split[0], tF_split[1])
-    if full_tF not in touchedDirs: touchedDirs.append(full_tF)   
+    tF_split = tF.split("/")
+    if len(tF_split) >= 1:
+        full_tF = os.path.join(tF_split[0],tF_split[1])
+        if full_tF not in touchedDirs: touchedDirs.append(full_tF)
+
+for tD in touchedDirs:
+    entry = []
+    for tF in touchedFiles:
+        if tD in tF: entry.append(tD)
+    touched[tD] = entry
+
+for key in touched:
+    print(key)
+    print(touched[key])
     potential = ""
-    if "games" in tF_split[0]:
-      print("this is a game: '%s'"%tF_split[1])
+    if "games" in key:
+      print("this is a game: '%s'"%key)
       potential = r"2. Implementation\14. Data\TP_config.json"
-    elif "casino" in tF_split[0]:
-      print("this is a casino: '%s'"%tF_split[1])
+    elif "casino" in key:
+      print("this is a casino: '%s'"%key)
       potential = r"2. Implementation\14. Data\TP_config.json"
     else:
       print("this isn't valid and should be ignored.")
 
-    full_tF = os.path.join(full_tF, potential)
-    print(full_tF)
-    if not os.path.exists(full_tF):
+    full_tf = os.path.join(key, potential)
+    print(full_tf)
+    if not os.path.exists(full_tf):
       print("TP_config file not found.  should make one?")
     else:
       print("TP_config file found.  does contain this file in assets?")
-
-print("touched dirs: [\n%s\n]"%(',\n'.join(touchedDirs))) 
+    
+    for tf in touched[key]:
+        print("'%s' exists: %s"%(touched[key], os.path.exists(touched[key])))
+    
